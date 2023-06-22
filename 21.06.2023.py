dc153a61a8586a0f7ea14142235b2b37a3b07d51
Ekaterina Myasnikova <keti200901@mail.ru>
2023-06-22 22:41:36 +0300

    new information in file

--- 21.06.2023.py
+++ 21.06.2023.py
@@ -1,3 +1,4 @@
+# Калькулятор 
 from colorama import Fore, Back, Style
 
 print(Fore.YELLOW)
@@ -20,3 +21,25 @@ elif what == "-":
 
 else:
   print("Выбрана неверная операция!")
+
+
+# Прогноз погоды
+from pyowm import OWM
+from pyowm.utils.config import get_default_config
+config_dict = get_default_config()
+config_dict['language'] = 'ru'
+owm = OWM('a4274abe4e931706f8097b83310bfbb3', config_dict)
+mgr = owm.weather_manager()
+place = input('Введите город: ')
+
+observation = mgr.weather_at_place(place)
+w = observation.weather
+
+temp = w.temperature('celsius')["temp"]
+hum = w.humidity
+wind = w.wind()["speed"]
+
+print("В городе " + place + " сейчас " + w.detailed_status + ".")
+print("Температура около " + str(round(temp)) + "°C")
+print("Влажность воздуха " + str(hum) + "%")
+print("Скорость ветра " + str(wind) + "м/с")
