40dd7cbf29c7e47fe445338fb7495204247a35be
Ekaterina Myasnikova <keti200901@mail.ru>
2023-08-07 17:21:47 +0300

    hexlet 13/15

---
+++ 07.08.2023.py
@@ -0,0 +1,27 @@
+# 13. Больше о декораторах
+# Упражнение:
+
+from functools import wraps
+
+
+def memoizing(max_):
+    def wrapper(func):
+        list_of_keys = []
+        list_of_value = []
+
+        @wraps(func)
+        def inner(x):
+            if x in list_of_keys:
+                index = list_of_keys.index(x)
+                return list_of_value[index]
+
+            else:
+                y = func(x)
+                list_of_keys.append(x)
+                list_of_value.append(y)
+                if (len(list_of_keys) > max_):
+                    list_of_keys.pop(0)
+                    list_of_value.pop(0)
+                return y
+        return inner
+    return wrapper
