ac15b311254fa623111c252c89e84b968ec9c101
Ekaterina Myasnikova <keti200901@mail.ru>
2023-08-07 20:49:54 +0300

    fibonacci hexlet

--- 07.08.2023.py
+++ 07.08.2023.py
@@ -25,3 +25,11 @@ def memoizing(max_):
                 return y
         return inner
     return wrapper
+
+# Испытание: Фибоначчи (Hexlet)
+def fib(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return fib(n - 1) + fib(n - 2)
