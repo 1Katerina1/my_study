f55a76875a21cd5921200ddecef179f40c816a2b
Ekaterina Myasnikova <keti200901@mail.ru>
2023-08-10 00:43:47 +0300

    the hardest tasks about recursion

--- 09.08.2023.py
+++ 09.08.2023.py
@@ -78,3 +78,80 @@ def predskazanie(a,Level=0):
         a=str(s)
         return predskazanie(a, Level+1)
 print(*predskazanie('109')) # => 1 2
+
+
+#  14. Рекурсия. Что такое рекурсия.
+''' Рекурсия в программировании — это возможность дать определение функции,
+используя в процессе саму определяемую функцию. '''
+def factorial(n):
+    print(f'n = {n}')
+    if n <= 0: # условие, которое прекращает рекурсию
+        return 1
+    return n * factorial(n - 1)
+
+n = 5
+print(f'factorial of {n} = {factorial(n)}')
+
+# Переполнение стека
+''' Память для стека не бесконечна =) '''
+
+# Пример линейной рекурсии
+def collatz(n):
+    print(f'n = {n}')
+    if n == 1:
+        return True
+    if n % 2 == 0:
+        return collatz(n // 2)
+    return collatz(n * 3 + 1)
+
+n = 3
+print(f'Collatz of {n}')
+print(collatz(n))
+# Здесь в теле функции рекурсивных вызова два, но в каждом заходе используется только один.
+
+# Пример каскадной рекурсии
+def fibonacci(n):
+    print(n)
+    if n <= 2:
+        return 1
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+n = 8
+print(f'Fibonacci of {n} = {fibonacci(n)}')
+''' Здесь функция всегда вызывает себя два раза.
+Сначала будет два вызова себя, которые превратятся в четыре — два раза по два вызова.
+Затем в восемь — количество вызовов растет каскадно. '''
+
+
+# Упражнение:
+# Принимает список и возвращает его длину
+def length(list_, L=0):
+    if not list_:
+        return L
+    else:
+        return length(list_[1:], L + 1)
+print(length([1, 0, 87, 34, 2, 6, 8])) # => 7
+
+# Принимает два числа begin и end и возвращает перевернутый список всех чисел между. 
+# Для простоты договоримся, что begin <= end
+def reverse_range(begin, end, l=''):
+    if begin == end:
+        A = list(l + str(begin))
+        return list(map(int, A)) # map преобразовывает каждый элемент списка из str в int
+    if end > begin:
+        l = l + str(end)
+        return reverse_range(begin, end-1, l)
+
+print('1:', reverse_range(1, 1)) # => [1]
+print('2:', reverse_range(1, 2)) # => [2, 1]
+print('3:', reverse_range(1, 3)) # => [3, 2, 1]
+
+# Принимает список чисел и возвращает новый только с положительными элементами
+def filter_positive(L):
+    if not L:
+        return L
+    if L[0] < 0:
+        return filter_positive(L[1:])
+    return [L[0]] + filter_positive(L[1:])
+  
+print(filter_positive([-1, 0, -2, 4, 8])) # => [0, 4, 8]
